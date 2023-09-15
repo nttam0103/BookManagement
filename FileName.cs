471689a74726765e3aa02fddfe702f4af088081a
Trong Tam <trongtam068@gmail.com>
2023-09-15 23:36:51 +0700

    Router(2) lớp nội bộ, ngoại lệ

--- FileName.cs
+++ FileName.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,25 +10,109 @@ namespace BookMan.ConsoleApp
 {
     internal class FileName
     {
-        static void Main() {
-            Console.WriteLine("Break");
-            var i = 0;
-            while (true)
+  
+        /// <summary>
+        /// lớp biểu diễn hình hộp
+        /// </summary>
+            public double Length { get; set; }
+            public double Breadth { get; set; }
+            public double Height { get; set; }
+            public FileName() { }
+            public FileName(double length, double breadth, double height)
             {
-                if (i == 10)
-                  break;
-                Console.Write($"{i}\t");
-                i++;
+                Length = length;
+                Breadth = breadth;
+                Height = height;
             }
-            Console.WriteLine("Continue");
-            var j = 0;
-            do
+            /// <summary>
+            /// tính thể tích khối hộp
+            /// </summary>
+            public double Volume => Length * Breadth * Height;
+            // nạp chồng phép cộng
+            public static FileName operator +(FileName b, FileName c)
             {
-                j++;
-                if (j == 5) continue;
-                Console.Write($"{j}\t"); 
-            } while (j < 10);
-            Console.ReadLine();
+                FileName box = new FileName
+                {
+                    Length = b.Length + c.Length,
+                    Breadth = b.Breadth + c.Breadth,
+                    Height = b.Height + c.Height
+                };
+                return box;
+            }
+            // nạp chồng phép so sánh bằng
+            public static bool operator ==(FileName lhs, FileName rhs)
+            {
+                bool status = false;
+                if (lhs.Length == rhs.Length && lhs.Height == rhs.Height
+                   && lhs.Breadth == rhs.Breadth)
+                {
+                    status = true;
+                }
+                return status;
+            }
+            // nạp chồng phép so sánh khác
+            public static bool operator !=(FileName lhs, FileName rhs)
+            {
+                bool status = false;
+                if (lhs.Length != rhs.Length || lhs.Height != rhs.Height ||
+                   lhs.Breadth != rhs.Breadth)
+                {
+                    status = true;
+                }
+                return status;
+            }
+            // nạp chồng phép so sánh nhỏ hơn
+            public static bool operator < (FileName lhs, FileName rhs)
+            {
+                bool status = false;
+                if (lhs.Length < rhs.Length && lhs.Height < rhs.Height
+                   && lhs.Breadth < rhs.Breadth)
+                {
+                    status = true;
+                }
+                return status;
+            }
+            // nạp chồng phép so sánh lớn hơn
+            public static bool operator >(FileName lhs, FileName rhs)
+            {
+                bool status = false;
+                if (lhs.Length > rhs.Length && lhs.Height >
+                   rhs.Height && lhs.Breadth > rhs.Breadth)
+                {
+                    status = true;
+                }
+                return status;
+            }
+            public override string ToString()
+            {
+                return string.Format("({0}, {1}, {2})", Length, Breadth, Height);
+            }
         }
+        internal class Program
+        {
+            private static void Main(string[] args)
+            {
+                FileName  Box1 = new    FileName(6, 7, 5);
+                FileName Box2 = new FileName(12, 13, 10);
+                FileName Box3 = new FileName();
+                FileName Box4 = new FileName();
+                /* phép cộng hai hình hộp cho ra hình hộp khác có kích thước 
+                 * bằng tổng kích thước của hai hộp */
+                Box3 = Box1 + Box2;
+                Console.WriteLine("FileName3: {0}", Box3.ToString());
+                Console.WriteLine("Volume of Box3 : {0}", Box3.Volume);
+                // so sánh hai hình hộp
+                if (Box1 > Box2)
+                    Console.WriteLine("Box1 lớn hơn Box2");
+                else
+                    Console.WriteLine("Box1 không lớn hơn Box2");
+                if (Box3 == Box4)
+                    Console.WriteLine("Box3 bằng Box4");
+                else
+                    Console.WriteLine("Box3 không bằng Box4");
+                Console.ReadKey();
+            }
+        
     }
 }
+    
---
+++ Framework/Router.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMan.ConsoleApp.Framework
+{
+    /// <summary>
+    /// Lớp cho phép ánh xạ truy vấn với phương thức 
+    /// </summary>
+    internal class Router
+    {
+        /// <summary>
+        /// lớp xử lý truy vấn 
+        /// </summary>
+        private class Resquest {
+            /// <summary>
+            /// thành phần của truy vấn 
+            /// </summary>
+           public string Route { get; private set; }
+           /// <summary>
+           /// thành phần tham số của truy vấn 
+           /// </summary>
+            public Parameter Parameter { get; private set; }
+            public Resquest (string request)
+            {
+                Analyze(request); 
+            }
+            /// <summary>
+            /// phân tích truy vấn để tách ra thành phần lệnh và thành phần tham só 
+            /// </summary>
+            /// <param name="request"></param>
+            private void Analyze(string request)
+            {
+               // tìm xem trong chuổi truy vấn có tham số hay không 
+               var firstIndex = request.IndexOf('?');
+                // trường hợp truy vấn không chứa tham số 
+                if (firstIndex < 0)
+                {
+                    Route = request.ToLower().Trim();
+                }
+                // Trường hợp truy vấn chưa tham số 
+                else
+                {
+                    // nếu chuổi lối chỉ chứa tham số, không chứa route 
+                    if (firstIndex <= 1) throw new Exception("Invaild request parameter");
+                    // cắt chuổi truy vấn lấy mốc là ký tự ? 
+                    // sau phép toán này thu được mảng 2 phần tử: thứ nhất là route, thứ hai là chuổi parameter 
+                    var tokens = request.Split(new[] {'?' },2,StringSplitOptions.RemoveEmptyEntries);
+                    // route là thành phần lệnh của truy vấn 
+                    Route = tokens[0].Trim().ToLower();
+                    //parameterPart là thành phần tham số của truy vấn 
+                    var parameterPart = request.Substring(firstIndex);
+                    Parameter = new Parameter(parameterPart);
+                }
+            }
+        }
+
+    }
+}
