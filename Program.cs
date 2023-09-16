8adb55f0317aefa113e931dc1f2366343e2d6d64
Trong Tam <trongtam068@gmail.com>
2023-09-16 16:49:45 +0700

    Router(4): Phuong thuc co danh, lambda

--- Program.cs
+++ Program.cs
@@ -11,20 +11,33 @@ namespace BookMan.ConsoleApp
     using Controllers;
     internal class Program
     {
-      private  static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            var context = new SimpleDataAccess();
+            SimpleDataAccess context = new SimpleDataAccess();
             BookController controller = new BookController(context);
 
-            Router.Instance.Resgister("about", About);
-            Router.Instance.Resgister("help", Help);
+            Router r = Router.Instance;
+            r.Resgister("about", About);
+            r.Resgister("help", Help);
+            r.Resgister(route: "create",
+                action: p => controller.Create(),
+                help: "[create]\r\nnhập sách mới");
+            r.Resgister(route: "update",
+                action: p => controller.Update(p["id"].ToInt()),
+                help: "[update ? id = <value>]\r\ntìm và cập nhật sách");
+            r.Resgister(route: "list",
+                action: p => controller.List(),
+                help: "[list]\r\nhiển thị tất cả sách");
+            r.Resgister(route: "single",
+                action: p => controller.Single(p["id"].ToInt()),
+                help: "[single ? id = < value >]\r\nhiển thị một cuốn sách theo id");
             while (true)
             {
-                ViewHelp.Write("Request> ",ConsoleColor.Green);
+                ViewHelp.Write("# Request >>> ", ConsoleColor.Green);
                 string request = Console.ReadLine();
                 Router.Instance.Forward(request);
-                Console.ReadLine();
+                Console.WriteLine();
             }
         }
         private static void About(Parameter parameter)
@@ -33,6 +46,7 @@ namespace BookMan.ConsoleApp
             ViewHelp.WriteLine("by trongtam", ConsoleColor.Magenta);
 
         }
+
         private static void Help(Parameter parameter)
         {
             if(parameter == null)
@@ -46,5 +60,7 @@ namespace BookMan.ConsoleApp
             var command = parameter["cmd"].ToLower();
             ViewHelp.WriteLine(Router.Instance.GetHelp(command));
         }
+  
+    
     }
 }
